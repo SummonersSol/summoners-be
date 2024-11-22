adc0a418e9d37045582c5dac84c24dfc7a7a5762
kida <leslug.away@gmail.com>
2024-11-23 00:29:49 +0800

    Fixed unable to update user

--- src/Models/User.ts
+++ src/Models/User.ts
@@ -1,9 +1,11 @@
 export type User = {
     id: number;
     address: string;
+    last_connected_at: string;
 }
 
 export const fillableColumns = [
     'id',
     'address',
+    'last_connected_at',
 ];
